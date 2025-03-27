f469375ebfa5f15a6ee1c6eef53a86d3b62d40fb
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-03-27 13:53:02 +0100

    Update index.php for docker use

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <?php
-        session_start();
+<?php
+    session_start();
 
-        $_SESSION['imgSrc'] = "";
-        $_SESSION['user'] = "";
+    $_SESSION['imgSrc'] = "";
+    $_SESSION['user'] = "";
 
-        header("Location: login.php");
-        exit();
-    ?>
-</body>
-</html>
+    header("Location: login.php");
+    exit();
+?>
